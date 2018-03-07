03b9c434a51e2b3355f34fafda821f01dbdf9b60
dac098 <dac098git@gmail.com>
2018-03-07 11:38:31 -0800

    added steps to setup the server and the socket

--- server/index.js
+++ server/index.js
@@ -1,7 +1,18 @@
+const process = require('process');
+
 const setup = require('modules/setup');
 const log = require('modules/log');
+
 (async () => {
 	log.setName('server');
 
+	log.info('processing cli arguments');
+	setup.processCliArgs();
+
+	log.info('starting server');
 	const server = require('./main');
+
+	if(process.env.NODE_ENV === 'development') {
+		require('./dev');
+	}
 })();
--- socket/index.js
+++ socket/index.js
@@ -1,7 +1,18 @@
+const process = require('process');
+
 const setup = require('modules/setup');
 const log = require('modules/log');
 
 (async () => {
 	log.setName('socket');
+
+	log.info('processing cli arguments');
+	setup.processCliArgs();
+
+	log.info('starting server');
 	let socket = require('./main');
+
+	if(process.env.NODE_ENV === 'development') {
+		require('./dev');
+	}
 })();
