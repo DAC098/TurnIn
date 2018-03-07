efa835393a884a0e4888ee721fbcde549bc5514e
dac098 <dac098git@gmail.com>
2018-03-07 11:37:54 -0800

    added dev files
     - idea is that if running in a docker-compose file then docker will restart the process and all that needs to happen is for the container to exit

---
+++ socket/dev.js
@@ -0,0 +1,53 @@
+const path = require('path');
+const process = require('process');
+
+const chokidar = require('chokidar');
+
+const log = require('modules/log');
+
+let timer = null;
+let watch_dir = path.resolve(__dirname,'../');
+let change_list = [];
+
+const shouldRestart = list => {
+	for(let file of list) {
+		if(file in require.cache) {
+			return true;
+		}
+	}
+
+	return false;
+};
+
+log.debug('watching directory',{dir:watch_dir});
+
+const watcher = chokidar.watch(watch_dir,{
+	ignoreInitial: true,
+	ignored: [
+		'**/node_modules/**',
+		'**/.git/**',
+		'**/.idea/**'
+	]
+});
+
+watcher.on('error', err => {
+	log.error(err.stack);
+});
+
+watcher.on('change', p => {
+	change_list.push(p);
+
+	if(timer)
+		clearTimeout(timer);
+
+	timer = setTimeout(() => {
+		clearTimeout(timer);
+
+		if(shouldRestart(change_list)) {
+			log.info('closing');
+			process.exit(0);
+		}
+
+		change_list = [];
+	},750);
+});
