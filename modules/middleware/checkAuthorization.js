89dff83de10b9450b09a02e56ad02449b1505d8a
dac098 <dac098git@gmail.com>
2018-03-07 20:31:07 -0800

    checkAuthorization middleware
    added a simple authorization checker to parse the Authorization header in requests

---
+++ modules/middleware/checkAuthorization.js
@@ -0,0 +1,43 @@
+const setup = require('../setup');
+const log = require('../log');
+
+const atob = (string) => {
+	if(Buffer.byteLength(string) !== string.length)
+		throw new Error('bad string given');
+	return Buffer.from(string,'base64').toString('utf8');
+};
+
+const checkAuthorization = async (req,res) => {
+	if(typeof req.headers['authorization'] === 'string') {
+		let split = req.headers['authorization'].split(' ');
+
+		switch(split[0]) {
+			case "Basic":
+				let parsed = atob(split[1]).split(':');
+				let username = parsed[0];
+				let password = parsed.slice(1).join(':');
+
+				if(username !== 'server') {
+					res.writeHead(401,{'content-type':'application/json'});
+					await res.endJSONAsync({
+						'message': 'only server is allowed user'
+					});
+					return false;
+				}
+
+				if(password !== setup.getKey('security.secret')) {
+					res.writeHead(401,{'content-type':'application/json'});
+					await res.endJSONAsync({
+						'message': 'invalid password given in authorization header'
+					});
+					return false;
+				}
+				break;
+			case "Digest":
+
+				break;
+		}
+	}
+};
+
+module.exports = checkAuthorization;
