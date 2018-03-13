48f433eb2fb660e0d2ef9e028906a12677e8d2ff
dac098 <dac098git@gmail.com>
2018-03-13 15:48:43 -0700

    would help if there was content for the file

---
+++ psql/init/20_submissions.sql
@@ -0,0 +1,12 @@
+\connect turnin
+
+CREATE TABLE submissions (
+id            SERIAL PRIMARY KEY,
+student_id    INT NOT NULL,
+assignment_id INT NOT NULL,
+past_due      BOOL DEFAULT FALSE,
+sub_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
+CONSTRAINT unique_assignments UNIQUE (student_id)
+FOREIGN KEY (student_id) REFERENCES students(id),
+FOREIGN KEY (assignment_id) REFERENCES assignments(id)
+)
