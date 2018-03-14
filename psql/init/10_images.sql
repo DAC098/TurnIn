6ee449e3e60a7cb54e07db3b901cf2de9c6b3ca3
dac098 <dac098git@gmail.com>
2018-03-14 16:07:18 -0700

    added new type to show the state of an image

--- psql/init/10_images.sql
+++ psql/init/10_images.sql
@@ -1,15 +1,17 @@
 \connect turnin
 
 CREATE TYPE image_types AS ENUM ('custom','hub');
+CREATE TYPE image_statuses AS ENUM ('created','running','error');
 
 CREATE TABLE images (
-id          SERIAL PRIMARY KEY,
-image_owner INTEGER NOT NULL,
-image_name  VARCHAR,
-docker_id   VARCHAR UNIQUE,
-options     JSON,
-image_type  image_types NOT NULL,
-dockerfile  VARCHAR,
-image_url   VARCHAR,
+id           SERIAL PRIMARY KEY,
+image_owner  INTEGER NOT NULL,
+image_name   VARCHAR,
+docker_id    VARCHAR UNIQUE,
+options      JSON,
+image_type   image_types NOT NULL,
+image_status image_statuses,
+dockerfile   VARCHAR,
+image_url    VARCHAR,
 FOREIGN KEY (image_owner) REFERENCES users(id)
 );
