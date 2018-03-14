276ea009f0e7ec2518cf956b18e99db5055925ca
dac098 <dac098git@gmail.com>
2018-03-13 21:55:06 -0700

    bug fix for getting the current_timestamp

--- psql/init/20_submissions.sql
+++ psql/init/20_submissions.sql
@@ -5,8 +5,8 @@ id            SERIAL PRIMARY KEY,
 student_id    INT NOT NULL,
 assignment_id INT NOT NULL,
 past_due      BOOL DEFAULT FALSE,
-sub_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP(),
-CONSTRAINT unique_assignments UNIQUE (student_id)
+sub_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+CONSTRAINT unique_assignments UNIQUE (student_id),
 FOREIGN KEY (student_id) REFERENCES students(id),
 FOREIGN KEY (assignment_id) REFERENCES assignments(id)
 )
