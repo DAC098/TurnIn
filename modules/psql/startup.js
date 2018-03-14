aa68c707852c7624140b87700d11066db3839855
dac098 <dac098git@gmail.com>
2018-03-13 21:53:17 -0700

    modified db startup
    if the server is in development it will attempt to load the db with test data

--- modules/psql/startup.js
+++ modules/psql/startup.js
@@ -1,11 +1,21 @@
+const process = require('process');
+
 const db = require('./index');
 const log = require('../log');
 const security = require('../security');
 
+const createUser = require('./helpers/createUser');
+
+const File = require('../fs/File');
+
+const is_dev = process.env.NODE_ENV;
+
 const startup = async () => {
+	let con = null;
+
 	try {
 		let pool = db.getPool();
-		let con = await pool.connect();
+		con = await pool.connect();
 		let master_check_query = `select * from users where username = 'master'`;
 
 		let res = await con.query(master_check_query);
@@ -25,9 +35,24 @@ const startup = async () => {
 				log.warn('unable to create master user');
 			}
 		}
+
+		if(is_dev) {
+			let test_data = JSON.parse(await File.read(process.cwd() + '/psql/test_data.json'));
+
+			for(let test_user of test_data.users) {
+				try {
+					let result = await createUser({'type':'master'},test_user);
+				} catch(err) {
+
+				}
+			}
+		}
 	} catch(err) {
 		log.error(err.stack);
 	}
+
+	if(con)
+		con.release();
 };
 
 exports.startup = startup;
