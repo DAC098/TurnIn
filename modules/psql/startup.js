ef993cd273b28c7399434bde321f2c3c529cda98
dac098 <dac098git@gmail.com>
2018-03-15 15:42:41 -0700

    added a method to wait for a connection to the server given an interval and retries

--- modules/psql/startup.js
+++ modules/psql/startup.js
@@ -10,6 +10,41 @@ const File = require('../fs/File');
 
 const is_dev = process.env.NODE_ENV;
 
+const wait = time => {
+	return new Promise((resolve) => {
+		let timer = setTimeout(() => {
+			clearTimeout(timer);
+			resolve();
+		},time);
+	});
+};
+
+const awaitConnection = async (attempts = 3,interval = 1000 * 10) => {
+	let connected = false;
+	for(let i = 0; i < attempts; ++i) {
+
+		try {
+			let pool = db.getPool();
+			let con = await pool.connect();
+			connected = true;
+
+			con.release();
+
+			return true;
+		} catch(err) {
+			if(err.code !== 'ECONNREFUSED')
+				throw err;
+		}
+
+		if(!connected)
+			await wait(interval);
+	}
+
+	return false;
+};
+
+exports.awaitConnection = awaitConnection;
+
 const startup = async () => {
 	let con = null;
 
@@ -25,7 +60,8 @@ const startup = async () => {
 			let salt = security.genSalt();
 			let password = security.genHash('password',salt);
 			let insert_master = `insert into users (username,password,salt,type) values
-			('master','${password}','${salt}','master')`;
+			('master','${password}','${salt}','master')
+			returning *`;
 
 			let res = await con.query(insert_master);
 
