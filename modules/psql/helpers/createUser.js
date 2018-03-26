30b346cf7d6635721a010a154143ff022ed75e82
dac098 <dac098git@gmail.com>
2018-03-25 22:12:14 -0700

    added comment and checks

--- modules/psql/helpers/createUser.js
+++ modules/psql/helpers/createUser.js
@@ -6,12 +6,29 @@ const security = require('../../security');
 
 const variables = require('../../variables');
 
-const createUser = async (user,body) => {
-	let con = null;
+/**
+ *
+ * @param user {Object}
+ * @param body {{
+ *     username: string,
+ *     password: string,
+ *     email: string=,
+ *     type: variables.user_type_map=,
+ *     is_student: boolean=,
+ *     is_teacher: boolean=,
+ *     fname: string=,
+ *     lname: string=,
+ *     permissions: variables.default_permissions=
+ * }}
+ * @param con  {SQLConnection=}
+ * @returns {Promise<{success: boolean,reason: string,user: Object|undefined}>}
+ */
+const createUser = async (user,body,con) => {
+	let given_con = con !== null && con !== undefined;
 
 	try {
-		let pool = db.getPool();
-		con = await pool.connect();
+		if(!given_con)
+			con = await db.connect();
 
 		let insert_fields = [];
 		let insert_values = [];
@@ -20,6 +37,9 @@ const createUser = async (user,body) => {
 			let username_check = await con.query(`select * from users where username = '${body.username}'`);
 
 			if(username_check.rows.length !== 0) {
+				if(!given_con)
+					con.release();
+
 				return {
 					success: false,
 					reason: 'username already used',
@@ -30,6 +50,9 @@ const createUser = async (user,body) => {
 			insert_fields.push('username');
 			insert_values.push(`'${body.username}'`);
 		} else {
+			if(!given_con)
+				con.release();
+
 			return {
 				success: false,
 				reason: 'no username given',
@@ -47,6 +70,9 @@ const createUser = async (user,body) => {
 			insert_fields.push('salt');
 			insert_values.push(`'${salt}'`);
 		} else {
+			if(!given_con)
+				con.release();
+
 			return {
 				success: false,
 				reason: 'no password given',
@@ -58,6 +84,9 @@ const createUser = async (user,body) => {
 			let email_check = await con.query(`select * from users where email = '${body.email}'`);
 
 			if(email_check.rows.length !== 0) {
+				if(!given_con)
+					con.release();
+
 				return {
 					success: false,
 					reason: 'email already used',
@@ -71,6 +100,9 @@ const createUser = async (user,body) => {
 
 		if(body.type) {
 			if(!(body.type in variables.user_type_map)) {
+				if(!given_con)
+					con.release();
+
 				return {
 					success: false,
 					reason: 'invalid user type given: ' + body.type,
@@ -79,6 +111,9 @@ const createUser = async (user,body) => {
 			}
 
 			if(variables.user_type_map[body.type] < variables.user_type_map[user.type]) {
+				if(!given_con)
+					con.release();
+
 				return {
 					success: false,
 					reason: 'you cannot create a user that is higher than you',
@@ -95,12 +130,12 @@ const createUser = async (user,body) => {
 
 		if(body.is_student) {
 			insert_fields.push('is_student');
-			insert_values.push(!!body.is_student);
+			insert_values.push(typeof body.is_student === 'boolean' ? body.is_student : !!body.is_student);
 		}
 
 		if(body.is_teacher) {
 			insert_fields.push('is_teacher');
-			insert_values.push(!!body.is_teacher);
+			insert_values.push(typeof body.is_teacher === 'boolean' ? body.is_teacher : !!body.is_teacher);
 		}
 
 		if(typeof body.fname === 'string' && typeof body.lname === 'string') {
@@ -136,19 +171,26 @@ const createUser = async (user,body) => {
 		let rows = util.createObject(result.rows);
 
 		if(rows.length === 1) {
+			if(!given_con)
+				con.release();
+
 			return {
 				success: true,
 				reason: '',
 				user: rows[0]
 			}
 		} else {
+			if(!given_con)
+				con.release();
+
 			return {
 				success: false,
-				reason: 'server was unable to create the user'
+				reason: 'server was unable to create the user',
+				user: undefined
 			}
 		}
 	} catch(err) {
-		if(con)
+		if(con && !given_con)
 			con.release();
 
 		throw err;
