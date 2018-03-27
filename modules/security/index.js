895de6d47b9a91f3b19e6b3d4a010ffa8354e481
dac098 <dac098git@gmail.com>
2018-03-27 12:35:16 -0700

    updated to include uuidv4 as well and fixed namespace

--- modules/security/index.js
+++ modules/security/index.js
@@ -1,11 +1,12 @@
 const nCrypto = require('crypto');
 
-const uuid = require('uuid/v5');
+const uuidV4 = require('uuid/v4');
+const uuidV5 = require('uuid/v5');
 
 const HASH = 'sha512';
 const ENCODING = 'utf-8';
 const BASE = 'base64';
-const NAMESPACE = 'turnin';
+const NAMESPACE = [12,84,52,34,124,63,74,95,120,210,184,2,16,14,132,23];
 
 const exp = {
 	genSalt: (size = 256,type = BASE) => {
@@ -17,8 +18,11 @@ const exp = {
 		hash.update(string);
 		return hash.digest(BASE);
 	},
-	uuid: (name,namespace = NAMESPACE) => {
-		return uuid(name,namespace);
+	uuidV5 : (name, namespace = NAMESPACE) => {
+		return uuidV5(name,namespace);
+	},
+	uuidV4 : (options,buffer,offset) => {
+		return uuidV4(options,buffer,offset);
 	}
 };
 
