694878eb87809fc318b77f9c03e91da067faffb6
dac098 <dac098git@gmail.com>
2018-03-26 17:55:03 -0700

    server updates
     - added listeners for closing and connections
     - added methods to attempt to safely close the server
     - issue with clients still being able to make requests

--- server/main.js
+++ server/main.js
@@ -1,5 +1,6 @@
 const http2 = require('http2');
 const fs = require('fs');
+const process = require('process');
 
 const setup = require('modules/setup');
 const log = require('modules/log');
@@ -21,6 +22,49 @@ server.on('listening',err => {
 		log.info('server listening for connections, https://0.0.0.0:443');
 });
 
+server.on('connection',async soc => {
+	log.info('connection',{total: await server.getConnectionsAsync()});
+});
+
+server.on('close',() => {
+	log.info('server closed, no active connections');
+});
+
+server.closeAsync = async function() {
+	return new Promise((resolve,reject) => {
+		this.close(err => {
+			if(err)
+				reject(err);
+			else
+				resolve();
+		});
+	});
+};
+
+server.getConnectionsAsync = function() {
+	return new Promise((resolve,reject) => {
+		this.getConnections((err,count) => {
+			if(err)
+				reject(err);
+			else
+				resolve(count);
+		});
+	});
+};
+
+server.closeAndExit = async function() {
+	log.warn('attempting to close and exit server',{connections: await this.getConnectionsAsync()});
+
+	try {
+		await this.closeAsync();
+		process.exit(0);
+	} catch(err) {
+		log.error(err.stack);
+	}
+};
+
 server.listen(443,'0.0.0.0');
+
+log.info('server',require('util').inspect(server,{depth:0}));
 
 module.exports = server;
