49428adfb70edde3304c6647dd38664cd561ad09
dac098 <dac098git@gmail.com>
2018-03-29 15:44:16 -0700

    changed name of column

--- psql/init/08_enrolled.sql
+++ psql/init/08_enrolled.sql
@@ -1,9 +1,9 @@
 \connect turnin
 
 CREATE TABLE enrolled (
-student_id INT NOT NULL,
+users_id   INT NOT NULL,
 section_id INT NOT NULL,
-CONSTRAINT pk_enrolled PRIMARY KEY (student_id,section_id),
-FOREIGN KEY (student_id) REFERENCES users(id),
+CONSTRAINT pk_enrolled PRIMARY KEY (users_id,section_id),
+FOREIGN KEY (users_id) REFERENCES users(id),
 FOREIGN KEY (section_id) REFERENCES sections(id)
 );
