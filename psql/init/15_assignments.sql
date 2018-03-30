39e4bc40573710ade03e03c179b45f9d2f57d972
dac098 <dac098git@gmail.com>
2018-03-29 20:31:39 -0700

    added more columns to assignments table

--- psql/init/15_assignments.sql
+++ psql/init/15_assignments.sql
@@ -1,8 +1,14 @@
 \connect turnin
 
 CREATE TABLE assignments (
-id         SERIAL PRIMARY KEY,
-title      VARCHAR NOT NULL,
-section_id INT NOT NULL,
-FOREIGN KEY (section_id) REFERENCES sections(id)
+id                  SERIAL PRIMARY KEY,
+title               VARCHAR NOT NULL,
+section_id          INT NOT NULL,
+description         VARCHAR,
+points              INT DEFAULT 0,
+open_date           TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
+close_date          TIMESTAMPTZ DEFAULT NULL,
+allow_custom_images BOOLEAN DEFAULT FALSE,
+FOREIGN KEY (section_id) REFERENCES sections(id),
+CONSTRAINT unique_title_section UNIQUE(title,section_id)
 );
