787863ae57659918053499f38444c68bc1f61b7a
dac098 <dac098git@gmail.com>
2018-03-30 16:40:30 -0700

    connection and session management
     - worked on trying to figure out how connections are managed in the server
     - sessions should be destroyed first then socket connections
     - if there is any connection that is open then the server will not close
     - need to check/test to see if a connection will destroy an h2 session

--- server/main.js
+++ server/main.js
@@ -15,84 +15,23 @@ const opts = {
 	allowHTTP1: true
 };
 let known_sessions = new Map();
-let known_connections = new Map();
+let known_sockets = new Map();
 
-const server = http2.createSecureServer(opts, handle);
-
-server.on('session',session => {
-	session.pk = security.uuidV4();
-	known_sessions.set(session.pk,session);
-
-	session.closeAsync = function() {
-		return new Promise((resolve, reject) => {
-			session.close(() => {
-				resolve();
-			});
-		});
-	};
-
-	session.on('close',() => {
-		log.debug('session close',{uuid:session.pk});
-		known_sessions.delete(session.pk);
-	});
-
-	session.on('error',err => {
-		log.error('session',err.stack);
-	});
-
-	session.on('timeout',() => {
-		log.debug('session timeout',{uuid:session.pk});
-	});
-
-	log.debug('session connection',{
-		uuid:session.pk,
-		remote:session.socket.remoteAddress,
-		local:session.socket.localAddress
-	});
-});
-
-server.on('connection',async soc => {
-	soc.pk = security.uuidV4();
-	known_connections.set(soc.pk,soc);
-
-	soc.on('close',() => {
-		log.debug('socket close',{uuid:soc.pk});
-		known_connections.delete(soc.pk);
-	});
-
-	soc.on('error',err => {
-		log.error('socket',err.stack);
-	});
-
-	soc.on('timeout',() => {
-		log.debug('socket timeout',{uuid:soc.pk});
-	});
-
-	log.debug('socket connection',{
-		uuid:soc.pk,
-		remote:soc.remoteAddress,
-		local:soc.localAddress
-	});
-});
-
-server.on('close',async () => {
-	log.info('server closed',{connections:await server.getConnectionsAsync()});
-});
+const server = http2.createSecureServer(opts);
 
 server.closeSessions = async () => {
 	for(let [pk,session] of known_sessions) {
-		if(!session.closed)
-			await session.closeAsync();
+		await session.closeAsync();
 	}
 };
 
-server.closeConnections = () => {
-	for(let [pk,soc] of known_connections) {
-		if(!soc.destroyed) {
-			soc.destroy();
+server.closeSockets = () => {
+	for(let [pk,socket] of known_sockets) {
+		if(!socket.destroyed) {
+			socket.destroy();
 		}
 
-		known_connections.delete(pk);
+		known_sockets.delete(pk);
 	}
 };
 
@@ -118,19 +57,17 @@ server.getConnectionsAsync = function() {
 	});
 };
 
+server.closeConnections = async function() {
+	await this.closeSessions();
+	await this.closeSockets();
+};
+
 server.closeAndExit = async function() {
-	log.warn('attempting to close and exit server',{
-		connections: await this.getConnectionsAsync()
-	});
-
 	try {
-		log.info('closing sessions');
-		await this.closeSessions();
+		await this.closeConnections();
 
-		log.info('closing connections');
-		this.closeConnections();
-
-		await this.closeAsync();
+		if(this.listening)
+			await this.closeAsync();
 
 		process.exit(0);
 	} catch(err) {
@@ -148,5 +85,99 @@ server.listenAsync = function (...args) {
 		});
 	});
 };
+
+server.on('request',async (req,res) => {
+	await handle(req,res);
+});
+
+/*
+there is a difference between the session and a connection.
+the session can be closed / destroyed but it is still possible
+for a connection to exist.
+example: if a request from chrome is made, the connection will
+stay open while the chrome is open only when chrome is closed will
+the connection also close (as of right now)
+ */
+server.on('session',session => {
+	session.pk = security.uuidV4();
+	known_sessions.set(session.pk,session);
+
+	session.closeAsync = function() {
+		return new Promise((resolve, reject) => {
+			if(!this.closed && !this.destroyed) {
+				this.close(() => {
+					known_sockets.delete(this.pk);
+					resolve();
+				});
+			} else {
+				known_sockets.delete(this.pk);
+				resolve();
+			}
+		});
+	};
+
+	session.on('close',async () => {
+		log.debug('session close',{
+			uuid:session.pk
+		});
+
+		known_sessions.delete(session.pk);
+	});
+
+	session.on('error',err => {
+		log.error('session',err.stack);
+	});
+
+	session.on('timeout',() => {
+		log.debug('session timeout',{uuid:session.pk});
+	});
+
+	session.on('connect',(...args) => {
+		log.debug('session connect',args);
+	});
+
+	log.debug('session connection',{
+		uuid:session.pk,
+		remote:session.socket.remoteAddress,
+		local:session.socket.localAddress
+	});
+});
+
+server.on('connection',async socket => {
+	socket.pk = security.uuidV4();
+	known_sockets.set(socket.pk,socket);
+
+	socket.on('close',() => {
+		log.debug('socket close',{uuid:socket.pk});
+		known_sockets.delete(socket.pk);
+	});
+
+	socket.on('error',err => {
+		log.error('socket',err.stack);
+	});
+
+	socket.on('timeout',() => {
+		log.debug('socket timeout',{uuid:socket.pk});
+	});
+
+	log.debug('socket connection',{
+		uuid:socket.pk,
+		remote:socket.remoteAddress,
+		local:socket.localAddress
+	});
+});
+
+server.on('close',async () => {
+	let connections = await server.getConnectionsAsync();
+
+	log.info('server closed',{
+		connections
+	});
+
+	if(connections > 0) {
+		log.info('attempting to close connections');
+		await server.closeConnections();
+	}
+});
 
 module.exports = server;
