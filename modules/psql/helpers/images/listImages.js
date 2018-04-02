0a17135024fa34c6e9080074a7904b6cdfbfcb41
dac098 <dac098git@gmail.com>
2018-04-02 15:50:33 -0700

    updated listImages
     - removed the check for the con and will now rely on it to be there
     - changed below_user and above_user to proper checks

--- modules/psql/helpers/images/listImages.js
+++ modules/psql/helpers/images/listImages.js
@@ -1,4 +1,4 @@
-const db = require('../../index');
+const log = require('../../../log');
 
 /**
  *
@@ -11,81 +11,66 @@ const db = require('../../index');
  *     limit: number,
  *     offset: number
  * }}
- * @param con     {SQLConnection=}
+ * @param con     {SQLConnection}
  * @returns {Promise<Array<Object>>}
  */
 const listImages = async (options,con) => {
-	let given_con = con !== null && con !== undefined;
+	let where = [],
+	    query = `
+	select
+		images.*,
+		users.username as username`;
 
-	try {
-		if(!given_con)
-			con = await db.connect();
-
-		let where = [],
-		    query = `
-		select
-			images.*,
-			users.username as username`;
-
-		if(typeof options.check_owner === 'number') {
-			query += `,
-			case when images.image_owner = ${options.check_owner} then TRUE else FALSE end as is_owner`;
-		}
-
-		query += `
-		from images
-		join users on
-			images.image_owner = users.id
-		`;
-
-		if(options.id) {
-			where.push(`images.id = ${options.id}`);
-		}
-
-		if(options.owner_id) {
-			where.push(`images.image_owner = ${options.owner_id}`);
-		}
-
-		if(options.below_user) {
-			where.push(`users.type <= '${options.below_user}'`);
-		}
-
-		if(options.above_user) {
-			where.push(`users.type >= '${options.above_user}'`);
-		}
-
-		if(where.length !== 0) {
-			query += `where
-			${where.join (' and ')}
-		`;
-		}
-
-		query += `order by
-			images.image_name
-		`;
-
-		if(options.limit) {
-			query += `limit ${options.limit}
-		`;
-		}
-
-		if(options.offset) {
-			query += `offset ${options.offset}
-		`;
-		}
-
-		let result = await con.query(query);
-
-		if(!given_con)
-			con.release();
-
-		return result.rows;
-	} catch(err) {
-		if(con && !given_con)
-			con.release();
-
-		throw err;
+	if(typeof options.check_owner === 'number') {
+		query += `,
+		case when images.image_owner = ${options.check_owner} then TRUE else FALSE end as is_owner`;
 	}
+
+	query += `
+	from images
+	join users on
+		images.image_owner = users.id
+	`;
+
+	if(options.id) {
+		where.push(`images.id = ${options.id}`);
+	}
+
+	if(options.owner_id) {
+		where.push(`images.image_owner = ${options.owner_id}`);
+	}
+
+	if(options.below_user) {
+		where.push(`users.type >= '${options.below_user}'`);
+	}
+
+	if(options.above_user) {
+		where.push(`users.type <= '${options.above_user}'`);
+	}
+
+	if(where.length !== 0) {
+		query += `where
+		${where.join (' and ')}
+	`;
+	}
+
+	query += `order by
+		images.image_name
+	`;
+
+	if(options.limit) {
+		query += `limit ${options.limit}
+	`;
+	}
+
+	if(options.offset) {
+		query += `offset ${options.offset}
+	`;
+	}
+
+	let result = await con.query(query);
+
+	return result.rows;
 };
 
 module.exports = listImages;
