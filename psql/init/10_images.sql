00e166263a5ab408050dc0e34413049f15e116e7
dac098 <dac098git@gmail.com>
2018-04-02 15:48:47 -0700

    added ready value to image_statuses

--- psql/init/10_images.sql
+++ psql/init/10_images.sql
@@ -1,7 +1,7 @@
 \connect turnin
 
 CREATE TYPE image_types AS ENUM ('custom','hub','remote');
-CREATE TYPE image_statuses AS ENUM ('created','running','removed','error');
+CREATE TYPE image_statuses AS ENUM ('created','ready','running','removed','error');
 
 CREATE TABLE images (
 id           SERIAL PRIMARY KEY,
