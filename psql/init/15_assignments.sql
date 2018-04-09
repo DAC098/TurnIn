be75fffedd9b98dcceff7cf2ff2f29073470d97c
dac098 <dac098git@gmail.com>
2018-04-08 20:30:37 -0700

    added options column for assignments

--- psql/init/15_assignments.sql
+++ psql/init/15_assignments.sql
@@ -8,6 +8,7 @@ description         VARCHAR,
 points              INT DEFAULT 0,
 open_date           TIMESTAMPTZ DEFAULT CURRENT_TIMESTAMP,
 close_date          TIMESTAMPTZ DEFAULT NULL,
+options             JSON,
 allow_custom_images BOOLEAN DEFAULT FALSE,
 FOREIGN KEY (section_id) REFERENCES sections(id),
 CONSTRAINT unique_title_section UNIQUE(title,section_id)
@@ -21,7 +22,7 @@ CONSTRAINT assignment_filename_pk PRIMARY KEY (filename,assignment_id)
 );
 
 CREATE TABLE assignment_images (
-image_id INT NOT NULL,
+image_id      INT NOT NULL,
 assignment_id INT NOT NULL,
 FOREIGN KEY (image_id) REFERENCES images(id),
 FOREIGN KEY (assignment_id) REFERENCES assignments(id),
