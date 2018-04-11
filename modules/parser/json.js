6d6a9ce097b5e524492dd34695a2ecdccb0f316b
dac098 <dac098git@gmail.com>
2018-04-11 15:35:33 -0700

    cleaned up code

--- modules/parser/json.js
+++ modules/parser/json.js
@@ -1,33 +1,31 @@
-const parseJSON = stream => {
-	return new Promise((resolve,reject) => {
-		if(stream.readable) {
-			let body = '';
+const parseJSON = stream => new Promise((resolve,reject) => {
+	if(stream.readable) {
+		let body = '';
 
-			stream.on('data', chunk => {
-				body += chunk;
-			});
+		stream.on('data', chunk => {
+			body += chunk;
+		});
 
-			stream.on('end', () => {
-				try {
-					if(body.length)
-						resolve(JSON.parse(body));
-					else
-						resolve({});
-				} catch(err) {
-					reject(err);
-				}
-			});
-
-			stream.on('error', err => {
+		stream.on('end', () => {
+			try {
+				if(body.length)
+					resolve(JSON.parse(body));
+				else
+					resolve({});
+			} catch(err) {
 				reject(err);
-			});
+			}
+		});
 
-			if(stream.isPaused())
-				stream.resume();
-		} else {
-			resolve(null);
-		}
-	});
-}
+		stream.on('error', err => {
+			reject(err);
+		});
+
+		if(stream.isPaused())
+			stream.resume();
+	} else {
+		resolve(null);
+	}
+});
 
 module.exports = parseJSON;
--- modules/parser/text.js
+++ modules/parser/text.js
@@ -1,33 +1,31 @@
-const parseText = stream => {
-	return new Promise((resolve,reject) => {
-		if(stream.readable) {
-			let body = '';
+const parseText = stream => new Promise((resolve,reject) => {
+	if(stream.readable) {
+		let body = '';
 
-			stream.on('data', chunk => {
-				body += chunk;
-			});
+		stream.on('data', chunk => {
+			body += chunk;
+		});
 
-			stream.on('end', () => {
-				try {
-					if(body.length)
-						resolve(body);
-					else
-						resolve('');
-				} catch(err) {
-					reject(err);
-				}
-			});
-
-			stream.on('error', err => {
+		stream.on('end', () => {
+			try {
+				if(body.length)
+					resolve(body);
+				else
+					resolve('');
+			} catch(err) {
 				reject(err);
-			});
+			}
+		});
 
-			if(stream.isPaused())
-				stream.resume();
-		} else {
-			resolve(null);
-		}
-	});
-}
+		stream.on('error', err => {
+			reject(err);
+		});
+
+		if(stream.isPaused())
+			stream.resume();
+	} else {
+		resolve(null);
+	}
+});
 
 module.exports = parseText;
