8d6ba65ab72b81e5cde6523229fcd07673089fd2
dac098 <dac098git@gmail.com>
2018-04-11 15:39:36 -0700

    column updates
     - student_id -> users_id
     - options
     - comment

--- psql/init/20_submissions.sql
+++ psql/init/20_submissions.sql
@@ -2,11 +2,14 @@
 
 CREATE TABLE submissions (
 id            SERIAL PRIMARY KEY,
-student_id    INT NOT NULL,
+users_id      INT NOT NULL,
 assignment_id INT NOT NULL,
+image_id      INT NOT NULL,
 past_due      BOOL DEFAULT FALSE,
 sub_date      TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
-CONSTRAINT unique_assignments UNIQUE (student_id),
-FOREIGN KEY (student_id) REFERENCES users(id),
-FOREIGN KEY (assignment_id) REFERENCES assignments(id)
+options       JSON,
+comment       VARCHAR,
+FOREIGN KEY (users_id) REFERENCES users(id),
+FOREIGN KEY (assignment_id) REFERENCES assignments(id),
+FOREIGN KEY (image_id) REFERENCES images(id)
 );
