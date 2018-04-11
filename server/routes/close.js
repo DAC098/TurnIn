75d6483ccfed11a707e3aa1aba9929f93dec6966
dac098 <dac098git@gmail.com>
2018-04-11 15:43:57 -0700

    updated to send worker close request

--- server/routes/close.js
+++ server/routes/close.js
@@ -1,5 +1,6 @@
 const process = require('process');
 
+const wkr = require('modules/worker');
 const isJsonContent = require('modules/middleware/isJsonContent');
 const checkUserType = require('modules/security/middleware/checkUserType');
 const parseJson = require('modules/parser/json');
@@ -25,6 +26,11 @@ module.exports = [
 		isJsonContent(),
 		checkUserType('master'),
 		async (req, res) => {
+			/**
+			 * @type {{
+			 *     file_update:string|string[]
+			 * }}
+			 */
 			let body = await parseJson(req);
 			let prevent_close = req.url_parsed.searchParams.has('no_close');
 			let force_close = req.url_parsed.searchParams.has('force_close');
@@ -37,45 +43,61 @@ module.exports = [
 			else
 				msg = 'closing server';
 
+			await wkr.updateFiles({
+				files: body.file_update,
+				prevent_close,
+				force_close
+			});
+
 			if(body) {
 				if(typeof body.file_update !== 'undefined') {
-					res.writeHead(200, {'content-type': 'application/json'});
-
 					if((Array.isArray(body.file_update) && checkCacheWithList(body.file_update)) ||
 						(typeof body.file_update === 'string' && body.file_update in require.cache)
 					) {
-						await res.endJSON({
+						await res.endJSON(202,{
 							'message': msg
 						});
 
-						if(force_close)
+						if(force_close) {
 							process.exit(0);
-						if(!prevent_close)
-							await server.closeAndExit();
+						}
+
+						if(!prevent_close) {
+							await server.shutdown();
+							process.exit(0);
+						}
 					} else {
-						await res.endJSON({
+						await res.endJSON(400,{
 							'message': 'non server file, not closing server'
 						});
 					}
 				} else {
-					await res.endJSON({
+					await res.endJSON(202,{
 						'message': msg
 					});
 
-					if(force_close)
+					if(force_close) {
 						process.exit(0);
-					else if(!prevent_close)
-						await server.closeAndExit();
+					}
+
+					if(!prevent_close) {
+						await server.shutdown();
+						process.exit(0);
+					}
 				}
 			} else {
-				await res.endJSON({
+				await res.endJSON(202,{
 					'message': msg
 				});
 
-				if(force_close)
+				if(force_close) {
 					process.exit(0);
-				else if(!prevent_close)
-					await server.closeAndExit();
+				}
+
+				if(!prevent_close) {
+					await server.shutdown();
+					process.exit(0);
+				}
 			}
 		}
 	]
