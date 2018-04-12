2d426fb8eafe05aa342e01b3594233594cc5111d
dac098 <dac098git@gmail.com>
2018-04-12 15:19:19 -0700

    run update
    the request can now create, execute, stop, and destroy a container instance that is given from an assignment and submission

--- worker/routes/run.js
+++ worker/routes/run.js
@@ -2,6 +2,7 @@ const db = require('modules/psql');
 const log = require('modules/log');
 
 const containers = require('modules/docker/containers');
+const exec = require('modules/docker/exec');
 const parser = require('modules/parser');
 const isJsonContent = require('modules/middleware/isJsonContent');
 
@@ -71,12 +72,107 @@ router.addRoute(route_info,isJsonContent(), async (req,res) => {
 	}
 
 	log.info('assignment',assignment_info);
+
 	log.info('submission',submission_info);
+
+	if(submission_info.image.type !== 'hub') {
+		log.warn('image is not on the hub');
+	}
+
+	let run_container = null;
 
 	log.info('creating container');
 
 	try {
-		let result = await containers.create(null,{});
+		let result = await containers.create(null,{
+			Image: submission_info.image.name,
+			Cmd: ['/bin/bash'],
+			Tty: false,
+			AttachStdin: true,
+			AttachStdout: true,
+			AttachStderr: true,
+			OpenStdin: true
+		});
+
+		log.info('container result',result);
+
+		if(result.success) {
+			run_container = result.returned
+		} else {
+			log.warn('unable create container, ending process');
+			return;
+		}
+	} catch(err) {
+		log.error(err.stack);
+	}
+
+	log.info('starting container');
+
+	try {
+		let result = await containers.start(run_container.Id);
+
+		log.info('starting result',result);
+	} catch(err) {
+		log.error(err.stack);
+	}
+
+	log.info('running executions');
+
+	for(let cmd of assignment_info.options.exec) {
+		log.info(`executing: "/bin/sh -c ${cmd}"`);
+
+		let exec_instance = null;
+
+		log.info('creating execute instance');
+
+		try {
+			let result = await exec.make(run_container.Id,{
+				AttachStdout: true,
+				AttachStderr: true,
+				Cmd: ['/bin/sh','-c',cmd]
+			});
+
+			log.info('exec create result',result);
+
+			if(result.success) {
+				exec_instance = result.returned
+			} else {
+				log.warn('failed to create exec instance');
+				continue;
+			}
+		} catch(err) {
+			log.error(err.stack);
+		}
+
+		log.info('executing command');
+
+		try {
+			let result = await exec.start(exec_instance.Id);
+
+			log.info('exec start result',result);
+		} catch(err) {
+			log.error(err.stack);
+		}
+	}
+
+	log.info('stopping container');
+
+	try {
+		let result = await containers.stop(run_container.Id);
+
+		log.info('stopping result',result);
+	} catch(err) {
+		log.error(err.stack);
+	}
+
+	log.info('extracting data');
+
+	log.info('destroying container');
+
+	try {
+		let result = await containers.remove(run_container.Id);
+
+		log.info('removal result',result);
 	} catch(err) {
 		log.error(err.stack);
 	}
